1b9599ed434e23f7e9e826144bb7344b2c50f55d
Alex Egger <alex.egger96@gmail.com>
2016-12-16 11:56:24 +0100

    Add multiboot header

---
+++ mb_header.asm
@@ -0,0 +1,10 @@
+section .multiboot_header
+header_start:
+dd 0xe85250d6														; Magic Number (Multiboot 2)
+dw 0																; Architecture (x86)
+dd header_end - header_start										; Header length
+dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))		; Checksum
+dw 0																; Type
+dw 0																; Flags
+dd 8																; Size
+header_end:
