7270b7de12a99d5817c2d7be0c8851043b0e2a64
Alex Egger <alex.egger96@gmail.com>
2016-12-20 12:23:12 +0100

    Add error function

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,17 +1,25 @@
 section .bss
 align 16
 stack_bottom:
-resw 16384 ; Reserve 16KB for the stack.
+	; Reserve 16KB for the stack.
+	resw 16384
 stack_top:
 
 section .text
 ; The kernel entry point.
 global _start
-
 bits 32
 _start:
 	mov esp, stack_top ; Setup the stack pointer.
 
 	; Print "OK" to the screen.
-	mov dword[0xb8000], 0x2f4b2f4f
+	mov al, "1"
+	jmp error
+	hlt
+
+error:
+	mov dword[0xb8000], 0x0C520C45
+	mov dword[0xb8004], 0x0C3A0C52
+	mov dword[0xb8008], 0x0C200C20
+	mov  byte[0xb800A], al
 	hlt
