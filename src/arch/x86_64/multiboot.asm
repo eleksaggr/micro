69bffec822cf40a644f67b8c0300df91322829e3
Alex Egger <alex.egger96@gmail.com>
2017-07-03 22:49:14 +0200

    Fix formatting errors

--- src/arch/x86_64/multiboot.asm
+++ src/arch/x86_64/multiboot.asm
@@ -1,10 +1,10 @@
 section .mbheader
 header_start:
-	dd 0xe85250d6														; Magic Number (Multiboot 2)
-	dd 0																; Architecture (x86)
-	dd header_end - header_start										; Header length
-	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))		; Checksum
-	dw 0																; Type
-	dw 0																; Flags
-	dd 8																; Size
+	dd 0xe85250d6                                                   ; Magic Number (Multiboot 2)
+	dd 0								; Architecture (x86)
+	dd header_end - header_start		                        ; Header length
+	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))	; Checksum
+	dw 0							        ; Type
+	dw 0							        ; Flags
+	dd 8							        ; Size
 header_end:
